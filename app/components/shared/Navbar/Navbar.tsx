8831f0e7901b1aa0abbb289e6ac918dc7b195496
MRiDuL-ICE <abdulwahab22400@gmail.com>
2025-02-28 08:39:11 +0600

    navbar dropdown hover color, shadwo redesign

--- app/components/shared/Navbar/Navbar.tsx
+++ app/components/shared/Navbar/Navbar.tsx
@@ -8,7 +8,7 @@ import { Contact } from "lucide-react";
 
 const Navbar = () => {
   const links = (
-    <div className="flex gap-8 font-bold">
+    <div className="flex gap-8 font-bold ">
       <NavLink
         className={
           "flex gap-1 items-center hover:text-[#41C6FB] duration-300 transform transition-all"
@@ -49,7 +49,7 @@ const Navbar = () => {
     </div>
   );
   return (
-    <div className="w-full border-b-2 fixed z-50 shadow-md top-0">
+    <div className="w-full fixed z-50 shadow-md s top-0 backdrop-blur-sm">
       <div className="flex justify-between h-18 items-center w-10/12 mx-auto">
         <div>
           <h2 className="text-2xl font-bold">MRiDuL-ICE</h2>
