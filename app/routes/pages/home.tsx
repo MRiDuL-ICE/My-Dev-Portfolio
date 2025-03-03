b750e69c2c9a89aa61bd86eddc1ec3a17bff49ea
MRiDuL-ICE <abdulwahab22400@gmail.com>
2025-03-03 16:55:10 +0600

    site title has been changed

--- app/routes/pages/home.tsx
+++ app/routes/pages/home.tsx
@@ -3,7 +3,7 @@ import Banner from "~/components/sections/Banner";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "MRiDuL-ICE" },
+    { title: "MRiDuL-ICE - Personal Portfolio" },
     { name: "description", content: "Welcome to React Router!" },
   ];
 }
